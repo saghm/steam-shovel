1cd96e8bd06458991d4259135eea704a0ca92ec8
Saghm Rossi <saghmrossi@gmail.com>
2018-12-19 00:31:25 -0500

    print invdividual probabilities

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,83 @@
+#[allow(dead_code)]
 mod math;
 
+use std::ops::Add;
+
+use structopt::StructOpt;
+
+use crate::math::Fraction;
+
+#[derive(Debug, StructOpt)]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+struct Opt {
+    #[structopt(short = "s", long = "hand-size")]
+    hand_size: Option<u8>,
+    #[structopt(short = "l", long = "--lands")]
+    num_lands: u8,
+    #[structopt(raw(required = "true"))]
+    targets: Vec<u8>,
+}
+
+fn format_land_counts(counts: &[u8]) -> String {
+    // structopt/clap enforce that `counts` will not be empty.
+
+    if counts.len() == 1 {
+        return format!("{}", counts[0]);
+    }
+
+    if counts.len() == 2 {
+        return format!("{} or {}", counts[0], counts[1]);
+    }
+
+    format!(
+        "{}or {}",
+        &counts[..counts.len() - 1]
+            .into_iter()
+            .map(|c| format!("{}, ", c))
+            .collect::<String>(),
+        counts[counts.len() - 1]
+    )
+}
+
 fn main() {
-    println!(
-        "chance of getting 2-3 lands on a six card mulligan with 20 lands in the deck: {:.3}%",
-        math::chance_of_splits(6, &[2, 3], 20) * 100.0
+    let opt = Opt::from_args();
+
+    let hand_size = opt.hand_size.unwrap_or(7);
+
+    let results = math::chance_of_splits(
+        hand_size.into(),
+        &opt.targets
+            .iter()
+            .cloned()
+            .map(Into::into)
+            .collect::<Vec<_>>(),
+        opt.num_lands.into(),
     );
-    println!(
-        "chance of getting 1-4 lands on a six card mulligan with 20 lands in the deck: {:.3}%",
-        math::chance_of_splits(6, &[1, 2, 3, 4], 20) * 100.0
+
+    macro_rules! print_chance {
+        ($count:expr, $hand_size:expr, $num_lands:expr, $chance:expr) => {
+            println!(
+                "chance of {} lands in a {} card hand from a deck with {} lands: {:.3}%",
+                $count, $hand_size, $num_lands, $chance
+            )
+        };
+    }
+
+    for (i, result) in results.iter().enumerate() {
+        print_chance!(
+            opt.targets[i],
+            hand_size,
+            opt.num_lands,
+            result.eval() * 100.0
+        );
+    }
+
+    println!("---------------------------------------------------------------------");
+
+    print_chance!(
+        format_land_counts(&opt.targets),
+        hand_size,
+        opt.num_lands,
+        results.iter().map(Fraction::eval).fold(0.0, Add::add) * 100.0
     );
 }
--- src/math.rs
+++ src/math.rs
@@ -1,7 +1,29 @@
+use std::ops::Mul;
+
 const DECK_SIZE: u128 = 60;
 
+#[derive(Debug)]
+pub struct Fraction {
+    num: u128,
+    denom: u128,
+}
+
+impl Fraction {
+    fn new(num: u128, denom: u128) -> Self {
+        Self { num, denom }
+    }
+
+    fn one() -> Self {
+        Self::new(1, 1)
+    }
+
+    pub fn eval(&self) -> f64 {
+        self.num as f64 / self.denom as f64
+    }
+}
+
 fn mult_range(begin: u128, end: u128) -> u128 {
-    (begin..=end).into_iter().fold(1, std::ops::Mul::mul)
+    (begin..=end).into_iter().fold(1, Mul::mul)
 }
 
 fn mult_range_down_by(begin: u128, down: u128) -> u128 {
@@ -20,7 +42,7 @@ fn chance_of_split(
     hand_size: u128,
     hand_count_of_type: u128,
     deck_count_of_type: u128,
-) -> (u128, u128) {
+) -> Fraction {
     let hand_count_of_other = hand_size - hand_count_of_type;
     let deck_count_of_other = DECK_SIZE - deck_count_of_type;
 
@@ -29,14 +51,14 @@ fn chance_of_split(
     let n3 = mult_range_down_by(deck_count_of_other, hand_count_of_other);
     let d = mult_range_down_by(DECK_SIZE, hand_size);
 
-    (n1 * n2 * n3, d)
+    Fraction::new(n1 * n2 * n3, d)
 }
 
-pub fn chance_of_splits(hand_size: u128, hand_counts: &[u128], deck_count: u128) -> f64 {
+pub fn chance_of_splits(hand_size: u128, hand_counts: &[u128], deck_count: u128) -> Vec<Fraction> {
     hand_counts
         .into_iter()
-        .map(|n| divide(chance_of_split(hand_size, *n, deck_count)))
-        .fold(0.0, std::ops::Add::add)
+        .map(|c| chance_of_split(hand_size, *c, deck_count))
+        .collect()
 }
 
 fn divide((n, d): (u128, u128)) -> f64 {
