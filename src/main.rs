a367c977abb79a4334496663063310b075317ac6
Saghm Rossi <saghmrossi@gmail.com>
2018-12-19 19:16:46 -0500

    specify which probabilities to print

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,27 @@ use structopt::StructOpt;
 
 use crate::math::Fraction;
 
+const DEFAULT_HAND_SIZE: u8 = 7;
+
 #[derive(Debug, StructOpt)]
-#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ArgRequiredElseHelp"))]
 struct Opt {
-    #[structopt(short = "s", long = "hand-size")]
-    hand_size: Option<u8>,
-    #[structopt(short = "l", long = "--lands")]
+    /// The number of lands in the deck.
+    #[structopt(long = "num-lands", short = "l")]
     num_lands: u8,
-    #[structopt(raw(required = "true"))]
-    targets: Vec<u8>,
+    /// The size of the hand being drawn.
+    #[structopt(long = "hand-size", short = "s")]
+    hand_size: Option<u8>,
+    /// The number of lands in the hand to print the percentages for.
+    ///
+    /// May be either a comma-separated list of integers or the word 'all' (e.g. '1', '2,3,4',
+    /// 'all').
+    #[structopt(long = "counts", short = "c")]
+    counts: String,
 }
 
 fn format_land_counts(counts: &[u8]) -> String {
-    // structopt/clap enforce that `counts` will not be empty.
+    // `counts` will not be empty.
 
     if counts.len() == 1 {
         return format!("{}", counts[0]);
@@ -41,40 +49,47 @@ fn format_land_counts(counts: &[u8]) -> String {
 fn main() {
     let opt = Opt::from_args();
 
-    let hand_size = opt.hand_size.unwrap_or(7);
+    let hand_size = opt.hand_size.unwrap_or(DEFAULT_HAND_SIZE);
+
+    let counts: Vec<_> = if opt.counts == "all" {
+        (0..=hand_size).into_iter().collect()
+    } else {
+        opt.counts
+            .split(',')
+            .map(|c| {
+                u8::from_str_radix(c, 10).unwrap_or_else(|_| {
+                    eprintln!(
+                        "error: --count must be either 'all' or a comma-separated list of integers"
+                    );
+                    std::process::exit(1);
+                })
+            })
+            .collect()
+    };
 
     let results = math::chance_of_splits(
         hand_size.into(),
-        &opt.targets
-            .iter()
-            .cloned()
-            .map(Into::into)
-            .collect::<Vec<_>>(),
+        &counts.iter().cloned().map(Into::into).collect::<Vec<_>>(),
         opt.num_lands.into(),
     );
 
     macro_rules! print_chance {
         ($count:expr, $hand_size:expr, $num_lands:expr, $chance:expr) => {
             println!(
-                "chance of {} lands in a {} card hand from a deck with {} lands: {:.3}%",
+                "chance of {} lands in a {} card hand from a deck with {} lands: {:>7.3}%",
                 $count, $hand_size, $num_lands, $chance
             )
         };
     }
 
     for (i, result) in results.iter().enumerate() {
-        print_chance!(
-            opt.targets[i],
-            hand_size,
-            opt.num_lands,
-            result.eval() * 100.0
-        );
+        print_chance!(counts[i], hand_size, opt.num_lands, result.eval() * 100.0);
     }
 
-    println!("---------------------------------------------------------------------");
+    println!("----------------------------------------------------------------------");
 
     print_chance!(
-        format_land_counts(&opt.targets),
+        format_land_counts(&counts),
         hand_size,
         opt.num_lands,
         results.iter().map(Fraction::eval).fold(0.0, Add::add) * 100.0
